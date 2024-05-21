99431d3775f54225519d789bd738f3a26997e087
Kevin Isaac Cortez <kenichi003@gmail.com>
2024-05-21 18:05:37 +0800

    Initial commit

---
+++ src/test/java/com/kentech/habitreward/HabitRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentech.habitreward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
